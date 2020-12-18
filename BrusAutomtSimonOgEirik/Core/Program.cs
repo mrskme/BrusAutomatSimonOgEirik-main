58d30107c3467eea738240f6be2f86fdba0869bc
MrSkme <59914093+mrskme@users.noreply.github.com>
2020-12-18 10:34:12 +0100

    sdzaf

--- BrusAutomtSimonOgEirik/Core/Program.cs
+++ BrusAutomtSimonOgEirik/Core/Program.cs
@@ -6,16 +6,7 @@ namespace BrusAutomatSimonOgEirik
     class Program /*: VendingMachine*/
     {
         static void Main(string[] args)
-       {
-            //Implementer en brusautomat. Ulike drikker har ulik pris. Automaten har en lagerbeholdning.
-            //Man kan putte på mynter (1kr, 5kr, 10kr, 20kr). Man trykker på en knapp for en bestemt drikk
-            //- hvis man har puttet på nok får man denne og saldoen reduseres tilsvarende.
-            //Det finnes en knapp for å få gjeldende saldo tilbake. 
-            //en fabrikk som lager brusen
-            //En storage som lagrer brus og er mellomledd mellom factory og vending machine
-            //abstrakt method and override
-            //SjokoladeMachine
-
+        { 
             var drinksFactory = new DrinksFactory();
             var chocolateFactory = new ChocolateFactory();
 
@@ -37,10 +28,8 @@ namespace BrusAutomatSimonOgEirik
             Console.WriteLine(drinksMachine.ShowProductList());
 
             var response = drinksMachine.ChooseProduct("Urge Intense", "Plastic");
-            var response2 = drinksMachine.ChooseProduct("Solo", "Plastic");
 
             drinksMachine.SpitOutProductOrError(response);
-            drinksMachine.SpitOutProductOrError(response2);
             drinksMachine.ReturnCoins();
 
             Console.WriteLine(drinksMachine.ShowProductList());
